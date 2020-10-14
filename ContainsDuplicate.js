839a57b6817e4dc1fca7a84c45809941867820e4
Haaris Mian <mail@haarismian.com>
2020-10-13 23:59:32 -0400

    working on a few

---
+++ ContainsDuplicate.js
@@ -0,0 +1,11 @@
+var containsDuplicate = function(nums) {
+    let map = new Map()
+    
+    for (i=0;i<nums.length;i++){
+        if (map.has(nums[i])) return true
+        else {
+            map.set(nums[i])
+        }
+    } return false
+    
+    };
