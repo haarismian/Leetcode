519c77e5dc39e530609fe09fa16855f81eb94c14
Haaris Mian <mail@haarismian.com>
2020-10-13 22:31:17 -0400

    finish

--- TwoSum.js
+++ TwoSum.js
@@ -1,2 +1,16 @@
 // https://leetcode.com/problems/two-sum/
 
+var twoSum = function(nums, target) {
+    let map = new Map()
+    
+    for (i=0;i<nums.length;i++){
+        let complement = target-nums[i]
+        
+        if (map.has(complement)){
+            return ([map.get(complement), i])
+        } else {
+            map.set(nums[i],i)
+        }
+    }
+    
+};
